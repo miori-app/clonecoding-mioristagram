b6e6987b24ca57daec7dfe857448fb8165d4d403
iammiori <mioriii@naver.com>
2020-03-13 16:48:57 +0900

    fix : Add navigation tab

--- app/src/main/java/com/example/mioristargram/navigation/AlarmFragment.kt
+++ app/src/main/java/com/example/mioristargram/navigation/AlarmFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.mioristargram.R
-
+// Alarm Fragment
 class AlarmFragment : Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm,container,false)
